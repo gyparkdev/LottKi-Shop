dc66b79f97ee2c8c4fe8769f74a916e5e425e60e
[hyuneeee1] <[hyuni028@naver.com]>
2023-08-18 17:39:21 +0900

    branch test

--- src/main/java/com/example/demo/LottKiShopApplication.java
+++ src/main/java/com/example/demo/LottKiShopApplication.java
@@ -9,7 +9,7 @@ public class LottKiShopApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LottKiShopApplication.class, args);
 		System.out.println("박박박과 김김김");
-		System.out.println("김김김2");
+		System.out.println("김김김2 With Branch ");
 	}
 
 }
